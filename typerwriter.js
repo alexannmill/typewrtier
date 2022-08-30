b9711098f20761be7c336e15092ee79380d359d3
alexannmill <alexannmill@gmail.com>
2022-08-30 21:01:02 +0000

    adding typewritter function

--- typerwriter.js
+++ typerwriter.js
@@ -1,34 +1,14 @@
 const sentence = "hello there from lighthouse labs";
 
-let interval =0 
-
-
-  for (const char of sentence) {
-console.log(`setting timer to print ${char} in ${interval} ms`)
-    setTimeout(()  => {
-      process.stdout.write(char)
-    }, interval);
-    interval += 100
-  }
-
-  setTimeout(()  => {
-    process.stdout.write("\n")
+let interval = 0;
+for (const char of sentence) {
+  console.log(`setting timer to print ${char} in ${interval} ms`);
+  setTimeout(() => {
+    process.stdout.write(char);
   }, interval);
+  interval += 100;
+}
 
-  // process.stdout.write("\n")
-
-
-// const delayLetters = () => {
-//   for (const char of sentence) {
-//     setTimeout(()  => {
-//       process.stdout.write(char)
-//     }, 50);
-//   }
-// };
-
-// delayLetters(setTimeout(()  => {
-//   process.stdout.write(char)
-// }, 50);
-// );
-//exit loop delay and re enter into loop 
-//call back need to have time out 
+setTimeout(() => {
+  process.stdout.write("\n");
+}, interval);
